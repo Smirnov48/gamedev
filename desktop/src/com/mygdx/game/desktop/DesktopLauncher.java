7b00c9711aead152053b47bc20ce9bc65fe678d8
Maxim Gorovets <gorovec3@gmail.com>
2017-06-22 14:59:25 +0300

    gd3gdh
    gjhghjghj

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package ru.smirnov48.gamedev.desktop;
+package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.smirnov48.gamedev.MyGdxGame;
+import com.mygdx.game.SummerGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new SummerGame(), config);
 	}
 }
