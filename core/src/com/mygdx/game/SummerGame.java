99e3f3aba9540d8914673437831a5a5ee3007f6c
Ivan <vvhappyguy@gmail.com>
2017-06-24 16:15:35 +0300

    Simple Animation

--- core/src/com/mygdx/game/SummerGame.java
+++ core/src/com/mygdx/game/SummerGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Iokin.Animator;
 import com.mygdx.game.Iokin.World;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
@@ -13,16 +14,20 @@ import com.badlogic.gdx.InputProcessor;
 public class SummerGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	World world;
+	Animator animator;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();		
 		world = new World();
+		animator = new Animator();
+		animator.create();
 	}
 
 	@Override
 	public void render () {
 		world.render(batch);
+		animator.render();
 	}
 	
 	@Override
