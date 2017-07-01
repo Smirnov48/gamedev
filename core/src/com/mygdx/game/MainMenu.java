9b77f6d03c9361f26199dffdab8426ba6c51b90b
Alexander Smirnov <smirnov48@gmail.com>
2017-07-01 09:29:39 +0300

    Template menu

---
+++ core/src/com/mygdx/game/MainMenu.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class MainMenu extends ScreenAdapter {
+	Texture img;
+	
+	public MainMenu(SummerGame game) {
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		SummerGame.batch.begin();
+		SummerGame.batch.draw(img, 0, 0);
+		SummerGame.batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		img.dispose();
+	}
+}
