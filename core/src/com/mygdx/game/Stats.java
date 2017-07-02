f6da544156d9048fc3f5a95c5209ac03cc8ca84d
Ivan <vvhappyguy@gmail.com>
2017-07-02 22:15:15 +0300

    Adding fractions lines
    Refactoring Stats.java
    drawing lines of reputation

--- core/src/com/mygdx/game/Stats.java
+++ core/src/com/mygdx/game/Stats.java
@@ -2,23 +2,36 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Stats {
-	Texture hpLogo;
-	Texture fpLogo;
-	Texture dpLogo;
-	Texture fEast;
-	Texture fWest;
-	Texture fNorth;
+	Texture logos;
+	TextureRegion hpLogo;
+	TextureRegion fpLogo;
+	TextureRegion dpLogo;
+	Texture fractions;
+	Texture blueLine;
+	Texture redLine;
+	Texture yellowLine;
 	
 	public Stats(){
-		hpLogo = new Texture("stats/points.jpg") ;
+		logos = new Texture("stats/points.jpg");
+		fpLogo = new TextureRegion(logos,0,0,45,60);
+		
+		fractions = new Texture("stats/fractions.jpg");
+		blueLine = new Texture("stats/blueline.jpg");
+		redLine = new Texture("stats/redline.jpg");
+		yellowLine = new Texture("stats/yellowline.jpg");	
 	}
 	
 	public void render(SpriteBatch ibatch){
 		SpriteBatch batch = ibatch;
 		batch.begin();
-		batch.draw(hpLogo,10,10);
+		batch.draw(fpLogo,100,100);
+		batch.draw(fractions, 698,-15);
+		batch.draw(blueLine, 703, 35);
+		batch.draw(redLine, 743,35);
+		batch.draw(yellowLine,783,35);
 		batch.end();
 	}
 }
