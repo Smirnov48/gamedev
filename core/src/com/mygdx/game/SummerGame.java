57cb33ef9380b59ff73acd02f4dfc3f2d1b19b2b
Alexander Smirnov <smirnov48@gmail.com>
2017-07-08 10:19:57 +0300

    make GameScreen with gamecycle

--- core/src/com/mygdx/game/SummerGame.java
+++ core/src/com/mygdx/game/SummerGame.java
@@ -1,19 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.Player;
-import com.mygdx.summer.screens.Play;
 
 public class SummerGame extends Game {
 	public static SummerGame game;
 	public static SpriteBatch batch;
-	Stats stats;
-	Player player;
-	private Play play;
-	
+
 	public static void setScr(Screen scr) {
 		game.setScreen(scr);
 	}
@@ -24,23 +18,17 @@ public class SummerGame extends Game {
 	public void create () {
 		game = this;
 		batch = new SpriteBatch();
-		play = new Play();
 		setScreen(new GameScreen());
-		stats = new Stats();
-		player = new Player();
 	}
 
 	@Override
 	public void render () {
 		super.render();
-		//play.render(Gdx.graphics.getDeltaTime());
-		stats.render(batch, player);
 	}
 
 	@Override
 	public void dispose () {
 		super.dispose();
-		stats.dispose();
 	}
 	
 	@Override
