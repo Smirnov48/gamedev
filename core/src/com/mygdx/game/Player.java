7440075bb6f8132b442201a496d7e8e441556215
Ivan <vvhappyguy@gmail.com>
2017-07-08 12:52:43 +0300

    Removing useless code.

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -23,30 +23,9 @@ public class Player {
 
 	public Player() {
 
-		walkSheet = new Texture(Gdx.files.internal("sprite-animation4.png"));
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-				walkSheet.getWidth() / FRAME_COLS, walkSheet.getHeight()
-						/ FRAME_ROWS);
-
-		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-		int index = 0;
-		for (int i = 0; i < FRAME_ROWS; i++) {
-			for (int j = 0; j < FRAME_COLS; j++) {
-				walkFrames[index++] = tmp[i][j];
-			}
-		}
-
-		walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
-		stateTime = 0f;
 	}
 
 	public void draw() {
-		stateTime += Gdx.graphics.getDeltaTime();
-
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
-		SummerGame.batch.begin();
-		SummerGame.batch.draw(currentFrame, 350, 350, -50, 50);
-		SummerGame.batch.end();
 	}
 
 	public void dispose() {
